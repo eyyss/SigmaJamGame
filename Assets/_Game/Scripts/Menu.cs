7ba592dd50e8ceb17e2d0d9990cedf9789df957e
eys <75747082+eyyss@users.noreply.github.com>
2023-08-16 15:43:59 +0300

    menu yaptım

---
+++ Assets/_Game/Scripts/Menu.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Menu : MonoBehaviour
+{
+    public TMP_Dropdown QualityDropDown;
+    void Awake()
+    {
+        if (PlayerPrefs.HasKey("QualityLevel"))
+        {
+            QualityDropDown.value = PlayerPrefs.GetInt("QualityLevel",2);
+        }
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Office");
+    }
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+    public void SaveQuaility(int index)
+    {
+        PlayerPrefs.SetInt("QualityLevel", index);
+    }
+
+    public void SetQuaility(int index)
+    {
+        QualitySettings.SetQualityLevel(index);
+        SaveQuaility(index);
+    }
+}
---
+++ Assets/_Game/Scripts/MouseMenu.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseMenu : MonoBehaviour
+{
+    public float sensitivity = 1.0f; // Hassasiyet ayarı
+    public float maxRotation = 15.0f; // Maksimum rotasyon miktarı
+
+    private Vector2 rotation = Vector2.zero;
+    
+
+
+    private void Update()
+    {
+        rotation += new Vector2(-Input.GetAxis("Mouse Y"), Input.GetAxis("Mouse X")) * sensitivity;
+
+        rotation.x = Mathf.Clamp(rotation.x, -maxRotation, maxRotation);
+        rotation.y = Mathf.Clamp(rotation.y,-maxRotation, maxRotation);
+
+        transform.localRotation = Quaternion.Lerp(transform.localRotation, 
+            Quaternion.Euler(rotation.x, rotation.y, 0),Time.deltaTime*5);
+    }
+}
