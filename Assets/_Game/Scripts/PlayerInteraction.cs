d70e5f97c1eecf8ef529bd003a721c6db22ac45e
burakfor <cevirkentc@gmail.com>
2023-08-16 16:26:17 +0300

    bueu

---
+++ Assets/_Game/Scripts/PlayerInteraction.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInteraction : MonoBehaviour
+{
+    public float Distance;
+    public LayerMask layerMask;
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            if(Physics.Raycast(transform.position, transform.forward, out RaycastHit hit, Distance,layerMask ))
+            { 
+              if(hit.collider != null)
+              {
+                IClickable clickable = hit.collider.GetComponent<IClickable>(); 
+                if(clickable != null)
+                {
+                  clickable.OnClick();
+                }
+              }
+            }
+        }
+    }
+}
